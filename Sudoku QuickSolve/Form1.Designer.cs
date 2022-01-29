31304be40172af11232a2e950ba460e6932eeab7
Edimar Valentín Kery <edimar.valentin@upr.edu>
2022-01-29 17:02:04 -0400

    Final additions for first release

--- Sudoku QuickSolve/Form1.Designer.cs
+++ Sudoku QuickSolve/Form1.Designer.cs
@@ -26,8 +26,13 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
+        /// 
+
+
         private void InitializeComponent()
         {
+
+            // Creates the Sudoku grid
             int x = 40;
             int y = 50;
             for(int i = 0; i < 9; i++)
@@ -38,6 +43,7 @@
                     grid[i, j].Name = "textBox " + i + "U+002C" + j;
                     grid[i, j].Size = new System.Drawing.Size(20, 20);
                     grid[i, j].TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+                    grid[i, j].Font = new System.Drawing.Font("Microsoft Sans Serif", 10, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                     this.Controls.Add(grid[i, j]);
 
                     if(j % 3 == 0)
@@ -65,6 +71,8 @@
             
             this.title = new System.Windows.Forms.Label();
             this.validateBtn = new System.Windows.Forms.Button();
+            this.solveBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // title
@@ -80,15 +88,39 @@
             //
             // validateBtn
             //
-            this.validateBtn.AutoSize = true;
+            this.validateBtn.AutoSize = false;
             this.validateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.validateBtn.Location = new System.Drawing.Point(85, 250);
+            this.validateBtn.Location = new System.Drawing.Point(65, 250);
             this.Controls.Add(this.validateBtn);
             this.validateBtn.Name = "validateBtn";
-            this.validateBtn.Size = new System.Drawing.Size(50, 10);
+            this.validateBtn.Size = new System.Drawing.Size(160, 40);
             this.validateBtn.Text = "Validate";
             this.validateBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.validateBtn.Click += new System.EventHandler(isValidClick);
+            this.validateBtn.Click += new System.EventHandler(ValidClick);
+            //
+            // solveBtn
+            //
+            this.solveBtn.AutoSize = false;
+            this.solveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solveBtn.Location = new System.Drawing.Point(65, 290);
+            this.Controls.Add(this.solveBtn);
+            this.solveBtn.Name = "solveBtn";
+            this.solveBtn.Size = new System.Drawing.Size(160, 40);
+            this.solveBtn.Text = "Solve";
+            this.solveBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.solveBtn.Click += new System.EventHandler(SolveClick);
+            //
+            // clearBtn
+            //
+            this.clearBtn.AutoSize = false;
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(65, 330);
+            this.Controls.Add(this.clearBtn);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(160, 40);
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.clearBtn.Click += new System.EventHandler(ClearClick);
             // 
             // Form1
             // 
@@ -97,11 +129,11 @@
             this.ClientSize = new System.Drawing.Size(290, 430);
             this.Controls.Add(this.title);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Sudoku Quick Solver";
             this.ResumeLayout(false);
             this.PerformLayout();
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
 
         }
 
@@ -110,6 +142,9 @@
         private System.Windows.Forms.TextBox[,] grid = new System.Windows.Forms.TextBox[9, 9];
         private System.Windows.Forms.Label title;
         public System.Windows.Forms.Button validateBtn;
+        public System.Windows.Forms.Button solveBtn;
+        public System.Windows.Forms.Button clearBtn;
+
 
 
         public System.Windows.Forms.TextBox[,] getGrid()
--- Sudoku QuickSolve/Form1.cs
+++ Sudoku QuickSolve/Form1.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ * Written by Edimar Valentín Kery <edimar.valentin@upr.edu>
+ * 
+ * The porpuse of this project was to have a self made Sudoku solver app in my computer for whenever 
+ * I want to see the answer of a newspaper sudoku puzzle I might be solving.
+ * 
+ * Data structures like multi dymensional arrays and hashsets were put into practice.
+ * Recursion and backtracking were used to make the suduko solver feature
+ * 
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,12 +29,14 @@ namespace Sudoku_QuickSolve
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        public void isValidClick(object sender, EventArgs e)
+     /*
+     * ValidClick - Event Function
+     * 
+     * Validates if the grid has valid values according to Sudoku rules
+     * Outputs if it's valid or not
+     * 
+    */
+        public void ValidClick(object sender, EventArgs e)
         {
             if (validateBtn.Text != "Validate")
             {
@@ -40,7 +54,144 @@ namespace Sudoku_QuickSolve
                 }
             }
         }
+       /*
+       * SolveClick - Event Function
+       * 
+       * Solves the grid and outputs it if was sucessful or not
+       * 
+      */
+        public void SolveClick(object sender, EventArgs e)
+        {
+            if (solveBtn.Text == "Solve")
+            {
+                if (isValid(getGrid()))
+                {
+                    if (solveSudoku(getGrid()))
+                    {
+                        solveBtn.Text = "Solved!";
+                    }
+                    else
+                    {
+                        solveBtn.Text = "Can't solve";
+                    }
+                }
+                else
+                {
+                    solveBtn.Text = "Can't solve";
+                }
+            }
+            else
+            {
+                solveBtn.Text = "Solve";
+            }
+        }
+        /*
+         * Clearlick - Event Function
+         * 
+         * Clears and restarts the grid for more inputs by the user
+         * 
+        */
+        public void ClearClick(object sender, EventArgs e)
+        {
+            System.Windows.Forms.TextBox[,] grid = getGrid();
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    grid[i, j].Clear();
+                    grid[i, j].ReadOnly = false;
+                    grid[i, j].Font = new System.Drawing.Font("Microsoft Sans Serif", 10, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 
+                }
+            }
+        }
+
+        //isNumberInRow returns true if a number exist in the specified row
+        private bool isNumberInRow(System.Windows.Forms.TextBox[,] grid, int number, int row)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                if(grid[row, i].Text.Length >= 1 && int.Parse(grid[row, i].Text.ToString()) == number)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //isNumberInColumn returns true if a number exist in the specified column
+        private bool isNumberInColumn(System.Windows.Forms.TextBox[,] grid, int number, int column)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (grid[i, column].Text.Length >= 1 && int.Parse(grid[i, column].Text.ToString()) == number)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //isNumberInSubGrid return true if a number exist in it's local 3 * 3 sub grid.
+        private bool isNumberInSubGrid(System.Windows.Forms.TextBox[,] grid, int number, int row, int column)
+        {
+            int localRow = row - row % 3;
+            int localColumn = column - column % 3;
+
+            for(int i = localRow; i < localRow + 3; i++)
+            {
+                for(int j = localColumn; j < localColumn + 3; j++)
+                {
+                    if(grid[i, j].Text.Length >= 1 && int.Parse(grid[i, j].Text.ToString()) == number)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        //isValidPlacement is a helper method that returns true if a number complies with Sudoku rules in a given coordinate
+        private bool isValidPlacement(System.Windows.Forms.TextBox[,] grid, int number, int row, int column)
+        {
+            return !isNumberInRow(grid, number, row) && !isNumberInColumn(grid, number, column) && !isNumberInSubGrid(grid, number, row, column);
+        }
+
+        //solveSudoku will recursevily solve the empty or wrong rows in the puzzle
+        private bool solveSudoku(System.Windows.Forms.TextBox[,] grid)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int column = 0; column < 9; column++)
+                {
+                    if (grid[row, column].Text.Length == 0)
+                        { 
+                        for (int entry = 1; entry < 10; entry++)
+                        {
+                            if (isValidPlacement(grid, entry, row, column))
+                            {
+                                grid[row, column].Font = new System.Drawing.Font("Microsoft Sans Serif", 10, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                                grid[row, column].Text = entry.ToString();
+                                grid[row, column].ReadOnly = true;
+
+                                if (solveSudoku(grid))
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    grid[row, column].Text = "";
+                                }
+
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        //isValid will return true if a grid complies with Sudoku rules
         private bool isValid(System.Windows.Forms.TextBox[,] grid)
         {
             HashSet<string> visited = new HashSet<string>();
